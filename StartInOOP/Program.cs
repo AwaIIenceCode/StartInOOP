be299b07e89ac8aa46b15f13b11a550b1b8b8962
AwallenceCode <awallencecode@gmail.com>
2025-10-01 21:49:06 +0300

    was writing the first program

--- StartInOOP/Program.cs
+++ StartInOOP/Program.cs
@@ -1,13 +1,35 @@
 ﻿using System;
-class Test
+class Note
 {
+    private string text;
+    private bool isImportant;
+    public Note(string noteText)
+    {
+        text = noteText;
+        isImportant = false;
+    }
+    public void MarkAsImportant()
+    {
+        isImportant = true;
+    }
+    public void Print()
+    {
+        string status = isImportant ? "- Yes" : "- No";
+        Console.WriteLine($"Text {text}, Important {status}");
+    }
     
 }
-
-class MyClass
+class Program
 {
     static void Main()
     {
-        Test t;
+        Note text_1 = new Note("Hodl BTC!");
+        Note text_2 = new Note("Sell in May and go away");
+        
+        text_1.Print();
+        text_2.Print();
+        
+        text_1.MarkAsImportant();
+        text_1.Print();
     }
 }
